d4b2dbeadc2d4e1de9326ac7af6f9f9841e72d53
fedddot <idanrommm@gmail.com>
2025-03-20 18:24:44 +0000

    controller data inited

---
+++ controller/include/cnc_controller_data.hpp
@@ -0,0 +1,19 @@
+#ifndef	CNC_CONTROLLER_DATA_HPP
+#define	CNC_CONTROLLER_DATA_HPP
+
+namespace cnc_controller {
+	template <typename Tlength>
+	struct Vector {
+		Tlength x;
+		Tlength y;
+		Tlength z;
+	};
+
+	template <typename Tlength, Tduration>
+	struct LinearMovement {
+		Vector<Tlength> destination;
+		Tduration duration;
+	};
+}
+
+#endif // CNC_CONTROLLER_DATA_HPP
